b1d8f68713ee9b774090fb16556e318587e9638f
acmesds@somemail.com <acmesds@nowhere.org>
2019-07-01 17:08:06 -0500

    update

--- config.sh
+++ config.sh
@@ -5,6 +5,7 @@ export BASE=/local
 export HERE=`pwd`
 
 export PLUGIN_REPO=https://sc.appdev.proj.coe.ic.gov/analyticmodeling/
+export CODE_REPO=https://sc.appdev.proj.coe.ic.gov/analyticmodeling/
 export JIRA=https://usms.nga.ic.gov/jira
 export RAS=http://ras.nga.ic.gov
 export STANFORD=http://stanford.edu
