1f98f25ecf81e6b7a0ebe6ef30e2bcb31b443641
acmesds@somemail.com <acmesds@nowhere.org>
2019-11-26 14:10:49 -0600

    fix plots. url parsing

--- flex.js
+++ flex.js
@@ -3874,26 +3874,52 @@ SELECT.gen = function (req, res) {
 
 SELECT.costs = function (req,res) {
 	const {sql,query} = req;
-	const {years, lag, vms} = query;
+	const {years, lag, vms, ups} = query;
 	var 
+		floor = Math.floor,
 		docRate = 140e3/30 + 110*4,	// docs/yr chome+pulse 
+		$vm = 5e3, 		// $/yr vm cost
 		nre$ = 2*2*100e3, 		// nre costs assuming 2 yrs dev by 2 FTEs if insourced (x3 if outsourced)
 		doc$ = 100e3*(10/2e3), // $/doc assuming analyst spends 2 hrs/doc to create (conservative but should not include mission etc costs)
 		minYr = 60*24*365, // mins/yr
 		cycleT = 5, // mins
 		boostT = 1,  // mins per boost cycle
-		batch = 5e3, 	// docs/cycle
-		Rcycles = docRate * lag / batch,	// cycles/yr in R state
-		Ocycles = docRate * 1 / batch,	// cycles/yr in Oper state
-		vmU = n => (cycleT + boostT*n) / minYr, 	// cpu utilization with boosting overhead
-		vm$ = n => 5e3 * vms * vmU(n),	// $/cycle
+		batch = 5e3, 	// docs/cycle 
+		queue = floor(docRate * lag), 	// total docs initially queued
+		Rcycles = queue / batch,	// cycles/yr in R state
+		Ocycles = docRate / batch,	// cycles/yr in Oper state
+		vm$ = n => $vm * vms * (cycleT + boostT*n) / minYr,		// $/cycle assuming dockerized vms
 		labU = 0.1,		// % docs labelled
-		$nre = nre$ / years, 	//	simple amort
+		$nre = nre$ / years, 	//	$/yr simple amort
 		$lab = docRate * labU * 100e3 * (0.25/2e3),	// $/yr assuming analyst spends 1/4 hr to label
-		$acq = doc$*docRate; 	// doc acquistion
+		$acq = doc$*docRate, 	// $/yr doc acquistion
+		samples = ups * years,
+		dt = 1/ups,
+		cycles = 0,
+		t = 0,
+		docs = 0,
+		cost = 0,
+		acq$ = 0,
+		lab$ = 0,
+		proc$ = 0,
+		nre$ = 0;
 	
 		try {
-			res( $(years, (n,rtn) => rtn[n] = {ID:n, yr: n, proc: n ? vm$(n*Ocycles)  : vm$(Rcycles), lab: $lab, nre: $nre, acq: $acq} ));
+			res( $(samples, (k,rtn) => {
+				rtn[ k ] = {ID:k, yr: t, lambda: docRate, cycles: cycles, queue: queue, proc: proc$, lab: lab$, nre: nre$, acq: acq$}
+				t += dt;
+				queue += floor(docRate * dt);
+				acq$ += $acq * dt,
+				lab$ += $lab * dt,
+				nre$ += $nre * dt;
+				
+				if ( queue >= batch ) {
+					cycles++;
+					queue -= batch;
+					proc$ += vm$(cycles);
+				}
+				//Log(k,floor(t), cycles,queue);
+			}));
 		}
 		catch (err) {
 			res( new Error("bad parameters") );
